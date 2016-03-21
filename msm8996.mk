4c8d51d1537ec1ce41b0edef2eec7879be80a248
Praveen Chavan <pchavan@codeaurora.org>
2016-03-21 09:37:14 -0700

    msm8996-common: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    
    Bug: 27654762
    
    Change-Id: I9aba945e50bece5b94f4fd15e44bc84d50a41a0b

--- msm8996.mk
+++ msm8996.mk
@@ -289,6 +289,11 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libshims_rild_socket
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
