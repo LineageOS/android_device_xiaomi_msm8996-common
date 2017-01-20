b9ffe34258b10076c90b0827b855d20a35dd3dfe
crancocco <shulaibai@hustunique.com>
2017-01-20 21:01:17 +0800

    msm8996-common: liblights: Support natrium notification LED
    Change-Id: Ic78107d670edf2e4c82db4144c39738500cbc2cd

--- liblight/lights.c
+++ liblight/lights.c
@@ -84,8 +84,6 @@ static int hw_buttons;
 #define GREEN_BLINK_FILE "/sys/class/leds/green/blink"
 #define BLUE_BLINK_FILE "/sys/class/leds/blue/blink"
 
-#define RGB_BLINK_FILE "/sys/class/leds/rgb/rgb_blink"
-
 #define RAMP_SIZE 8
 static int BRIGHTNESS_RAMP[RAMP_SIZE] = { 0, 12, 25, 37, 50, 72, 85, 100 };
 #define RAMP_STEP_DURATION 50
@@ -296,8 +294,15 @@ static int set_speaker_light_locked(struct light_device_t* dev,
     blue = colorRGB & 0xFF;
     blink = onMS > 0 && offMS > 0;
 
+    // Bias for true white
+    if (colorRGB != 0 && red == green && green == blue) {
+        blue = (blue * 171) / 256;
+    }
+
     // Disable all blinking to start
-    write_int(RGB_BLINK_FILE, 0);
+    write_int(RED_BLINK_FILE, 0);
+    write_int(GREEN_BLINK_FILE, 0);
+    write_int(BLUE_BLINK_FILE, 0);
 
     if (blink) {
         stepDuration = RAMP_STEP_DURATION;
@@ -341,13 +346,10 @@ static int set_speaker_light_locked(struct light_device_t* dev,
         free(duty);
 
         // Start the party
-        write_int(RGB_BLINK_FILE, 1);
+        write_int(RED_BLINK_FILE, red);
+        write_int(GREEN_BLINK_FILE, green);
+        write_int(BLUE_BLINK_FILE, blue);
     } else {
-        if (red == 0 && green == 0 && blue == 0) {
-            write_int(RED_BLINK_FILE, 0);
-            write_int(GREEN_BLINK_FILE, 0);
-            write_int(BLUE_BLINK_FILE, 0);
-        }
         write_int(RED_LED_BRIGHTNESS_FILE, red);
         write_int(GREEN_LED_BRIGHTNESS_FILE, green);
         write_int(BLUE_LED_BRIGHTNESS_FILE, blue);
