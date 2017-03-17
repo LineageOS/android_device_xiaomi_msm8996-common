897e011c565d4d2d0a1395ac67c8449c70b7c433
LuK1337 <priv.luk@gmail.com>
2017-03-17 17:06:25 +0100

    msm8996-common: Disable ambient display
    Change-Id: I8c5a3c1572cc35b8f85403d7f63a88b4f0284b24

--- msm8996.mk
+++ msm8996.mk
@@ -155,10 +155,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:system/etc/calib.cfg
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprintd \
