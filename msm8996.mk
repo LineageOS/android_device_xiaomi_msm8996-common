5dcd68703d69946d817a146e8141d52583e60168
Luca Stefani <luca020400@lineageos.org>
2017-03-23 18:37:50 +0100

    msm8996-common: Remove cnss_diag & fstman
    * We never start cnss_diag service
    * We don't ship fstman binary, furthermore it's not needed
    
    Change-Id: I7fe9a139e2ddec33f595682f5d97ba386ccab57a

--- msm8996.mk
+++ msm8996.mk
@@ -294,5 +294,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
