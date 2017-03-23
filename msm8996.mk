8010f76f82489407156d5e603812fe22abb1b014
Luca Stefani <luca020400@lineageos.org>
2017-03-23 18:37:50 +0100

    msm8996-common: Remove unused blobs & configs
    * We never start cnss_diag & msm_irqbalance services
    * We don't ship fstman binary, furthermore it's not needed
    * We don't execute init.qcom.bt.sh
    
    Change-Id: I7fe9a139e2ddec33f595682f5d97ba386ccab57a

--- msm8996.mk
+++ msm8996.mk
@@ -130,8 +130,7 @@ PRODUCT_PACKAGES += \
 
 # Common config scripts
 PRODUCT_PACKAGES += \
-    init.panel.sh \
-    init.qcom.bt.sh
+    init.panel.sh
 
 # Common init scripts
 PRODUCT_PACKAGES += \
@@ -201,10 +200,6 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes \
     libebtc
-
-# IRQ Balancer
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
 # Lights
 PRODUCT_PACKAGES += \
@@ -294,5 +289,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
