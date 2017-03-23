428821ede64e75821e3332fd6250d62af21d55f3
Luca Stefani <luca020400@lineageos.org>
2017-03-23 18:17:38 +0100

    msm8996-common: rootdir: Rename init.leds to init.service
    * Also remove max_brightness leds cap
    
    Change-Id: Ib74f934c70ff5600b3703919ed18a39b07269a2c

--- rootdir/etc/init.panel.sh
+++ rootdir/etc/init.panel.sh
@@ -26,61 +26,31 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-# Update the panel color property and Leds brightness 
-for i in $(seq 5); do
-    if [ -f /sys/bus/i2c/devices/12-004a/panel_color ]; then
-	# Atmel mXT
-	color=`cat /sys/bus/i2c/devices/12-004a/panel_color`
-	if [ -n "$color" ]; then
-	    break
-	else
-	    sleep 1
-	    continue
-	fi
-    elif [ -f /sys/bus/i2c/devices/12-0020/panel_color ]; then
-	# Synaptics DSX
-	color=`cat /sys/bus/i2c/devices/12-0020/panel_color`
-	if [ -n "$color" ]; then
-	    break
-	else
-	    sleep 1
-	    continue
-	fi
-    else
-	color="0"
-	sleep 1
-    fi
-done
+# Set the panel color property
+if [ -f /sys/bus/i2c/devices/12-004a/panel_color ]; then
+    # Atmel
+    color=`cat /sys/bus/i2c/devices/12-004a/panel_color`
+elif [ -f /sys/bus/i2c/devices/12-0020/panel_color ]; then
+    # Synaptics
+    color=`cat /sys/bus/i2c/devices/12-0020/panel_color`
+else
+    color="0"
+fi
 
 case "$color" in
     "1")
         setprop sys.panel.color WHITE
-	echo 40 > /sys/class/leds/red/max_brightness
-        echo 60 > /sys/class/leds/green/max_brightness
-        echo 60 > /sys/class/leds/blue/max_brightness
         ;;
     "2")
         setprop sys.panel.color BLACK
-	echo 35 > /sys/class/leds/red/max_brightness
-        echo 75 > /sys/class/leds/green/max_brightness
-        echo 40 > /sys/class/leds/blue/max_brightness
         ;;
     "7")
         setprop sys.panel.color PURPLE
-	echo 40 > /sys/class/leds/red/max_brightness
-        echo 140 > /sys/class/leds/green/max_brightness
-        echo 120 > /sys/class/leds/blue/max_brightness
         ;;
     "8")
         setprop sys.panel.color GOLDEN
-	echo 45 > /sys/class/leds/red/max_brightness
-        echo 80 > /sys/class/leds/green/max_brightness
-        echo 70 > /sys/class/leds/blue/max_brightness
         ;;
     *)
         setprop sys.panel.color UNKNOWN
-	echo 35 > /sys/class/leds/red/max_brightness
-        echo 75 > /sys/class/leds/green/max_brightness
-        echo 40 > /sys/class/leds/blue/max_brightness
         ;;
 esac
