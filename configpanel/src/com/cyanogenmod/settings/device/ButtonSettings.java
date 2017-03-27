f484bf6920ad3ef5fc05d03f48bcc4a3b2dea397
Bruno Martins <bgcngm@gmail.com>
2017-03-27 19:25:28 +0000

    msm8996-common: Allow control over PocketMode service
     * Once the service is running, proximity sensor is constantly active
       when the display is turned off, resulting into a residual increase
       in battery consumption. Add a toggle so that users can decide whether
       they accept that and prefer to prevent accidental wake-ups triggered
       by the fingerprint sensor.
     * Only keep main service running if fingerprint wake-up feature is
       enabled at the same time as accidental wake-up prevention feature.
     * Set PocketMode as a required module of ConfigPanel, to make sure
       it is only shipped on devices building the latter.
    
    Change-Id: I5941f445d0bd77be1efa9ffcde2d9854e6446c3d

--- configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
@@ -52,6 +52,9 @@ public class ButtonSettings extends PreferenceActivity implements OnPreferenceCh
         if (!TextUtils.isEmpty(node)) {
             Boolean value = (Boolean) newValue;
             FileUtils.writeLine(node, value ? "1" : "0");
+            if (Constants.FP_WAKEUP_KEY.equals(preference.getKey())) {
+                Constants.broadcastCustIntent(this, value);
+            }
             return true;
         }
         node = Constants.sStringNodePreferenceMap.get(preference.getKey());
@@ -59,6 +62,12 @@ public class ButtonSettings extends PreferenceActivity implements OnPreferenceCh
             FileUtils.writeLine(node, (String) newValue);
             return true;
         }
+
+        if (Constants.FP_PROXIMITY_KEY.equals(preference.getKey())) {
+            Constants.broadcastCustIntent(this, (Boolean) newValue);
+            return true;
+        }
+
         return false;
     }
 
@@ -89,6 +98,10 @@ public class ButtonSettings extends PreferenceActivity implements OnPreferenceCh
                 l.setEnabled(false);
             }
         }
+
+        // Initialize other preferences whose keys are not associated with nodes
+        SwitchPreference b = (SwitchPreference) findPreference(Constants.FP_PROXIMITY_KEY);
+        b.setOnPreferenceChangeListener(this);
     }
 
     private void updatePreferencesBasedOnDependencies() {
