5c3c3e2b55d18f2ba38ceb1c0721bd6e7ea96fff
Bruno Martins <bgcngm@gmail.com>
2017-03-28 14:16:49 +0000

    msm8996-common: ConfigPanel: Clean up
     * Merge all code related to button settings into one single class
       and get rid of unused code
     * Remove unnecessary permissions
    
    Change-Id: Id49e3aff5a243201cff5177de78cca26d932a055

--- configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/ButtonSettings.java
@@ -15,16 +15,92 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package com.cyanogenmod.settings.device.utils;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
+import android.text.TextUtils;
 
-import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
+import java.io.File;
 
-public class ButtonSettings extends NodePreferenceActivity {
+import org.cyanogenmod.internal.util.FileUtils;
+
+public class NodePreferenceActivity extends PreferenceActivity
+        implements OnPreferenceChangeListener {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.button_panel);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updatePreferencesBasedOnDependencies();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String node = Constants.sBooleanNodePreferenceMap.get(preference.getKey());
+        if (!TextUtils.isEmpty(node)) {
+            Boolean value = (Boolean) newValue;
+            FileUtils.writeLine(node, value ? "1" : "0");
+            return true;
+        }
+        node = Constants.sStringNodePreferenceMap.get(preference.getKey());
+        if (!TextUtils.isEmpty(node)) {
+            FileUtils.writeLine(node, (String) newValue);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void addPreferencesFromResource(int preferencesResId) {
+        super.addPreferencesFromResource(preferencesResId);
+        // Initialize node preferences
+        for (String pref : Constants.sBooleanNodePreferenceMap.keySet()) {
+            SwitchPreference b = (SwitchPreference) findPreference(pref);
+            if (b == null) continue;
+            b.setOnPreferenceChangeListener(this);
+            String node = Constants.sBooleanNodePreferenceMap.get(pref);
+            if (new File(node).exists()) {
+                String curNodeValue = FileUtils.readOneLine(node);
+                b.setChecked(curNodeValue.equals("1"));
+            } else {
+                b.setEnabled(false);
+            }
+        }
+        for (String pref : Constants.sStringNodePreferenceMap.keySet()) {
+            ListPreference l = (ListPreference) findPreference(pref);
+            if (l == null) continue;
+            l.setOnPreferenceChangeListener(this);
+            String node = Constants.sStringNodePreferenceMap.get(pref);
+            if (new File(node).exists()) {
+                l.setValue(FileUtils.readOneLine(node));
+            } else {
+                l.setEnabled(false);
+            }
+        }
+    }
+
+    private void updatePreferencesBasedOnDependencies() {
+        for (String pref : Constants.sNodeDependencyMap.keySet()) {
+            SwitchPreference b = (SwitchPreference) findPreference(pref);
+            if (b == null) continue;
+            String dependencyNode = Constants.sNodeDependencyMap.get(pref)[0];
+            if (new File(dependencyNode).exists()) {
+                String dependencyNodeValue = FileUtils.readOneLine(dependencyNode);
+                boolean shouldSetEnabled = dependencyNodeValue.equals(
+                        Constants.sNodeDependencyMap.get(pref)[1]);
+                Constants.updateDependentPreference(this, b, pref, shouldSetEnabled);
+            }
+        }
     }
 }
