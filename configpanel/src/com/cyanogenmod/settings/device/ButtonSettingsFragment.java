3e9cc003bb140d07376e6ba3279a2cb51f1256f1
Bruno Martins <bgcngm@gmail.com>
2017-04-03 23:33:47 +0100

    msm8996-common: ConfigPanel: Check if PocketMode is installed
     * Because users like to play tricks
    
    Change-Id: I84c8d5032ef3df5ab2889e957d9a5b6fc270c091

--- configpanel/src/com/cyanogenmod/settings/device/ButtonSettingsFragment.java
+++ configpanel/src/com/cyanogenmod/settings/device/ButtonSettingsFragment.java
@@ -24,6 +24,7 @@ import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -31,13 +32,23 @@ import android.view.MenuItem;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 
 import org.cyanogenmod.internal.util.FileUtils;
+import org.cyanogenmod.internal.util.PackageManagerUtils;
 
 public class ButtonSettingsFragment extends PreferenceFragment
         implements OnPreferenceChangeListener {
 
+    private SwitchPreference mPocketModePreference;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.button_panel);
+
+        final PreferenceCategory fingerprintCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(Constants.CATEGORY_FP);
+        if (!PackageManagerUtils.isAppInstalled(getContext(), "com.cyanogenmod.pocketmode")) {
+            fingerprintCategory.removePreference(mPocketModePreference);
+            mPocketModePreference = null;
+        }
     }
 
     @Override
@@ -103,8 +114,8 @@ public class ButtonSettingsFragment extends PreferenceFragment
         }
 
         // Initialize other preferences whose keys are not associated with nodes
-        SwitchPreference b = (SwitchPreference) findPreference(Constants.FP_POCKETMODE_KEY);
-        b.setOnPreferenceChangeListener(this);
+        mPocketModePreference = (SwitchPreference) findPreference(Constants.FP_POCKETMODE_KEY);
+        mPocketModePreference.setOnPreferenceChangeListener(this);
     }
 
     private void updatePreferencesBasedOnDependencies() {
