9c4808c9d0497a57b5d502c261ced27e0105e39d
Luca Stefani <luca020400@lineageos.org>
2017-04-17 19:45:57 +0200

    msm8996-common: Assert on modem version
    * Trustzone isn't enough
    
    Change-Id: I5a49c9571bba603af7873222e5383ec0494619e4

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,10 +28,10 @@ def IncrementalOTA_Assertions(info):
 
 def AddTrustZoneAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(\S+)', android_info)
   if m:
     versions = m.group(1).split('|')
     if len(versions) and '*' not in versions:
-      cmd = 'assert(xiaomi.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+      cmd = 'assert(xiaomi.verify_modem(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
       info.script.AppendExtra(cmd)
   return
