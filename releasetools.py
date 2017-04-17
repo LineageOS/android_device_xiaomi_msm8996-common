5e151bf8fafba5db193d84f30fa0a9fd494ac36d
Luca Stefani <luca020400@lineageos.org>
2017-04-17 19:45:57 +0200

    msm8996-common: Assert on modem build time stap
    * Trustzone isn't enough
    * Do >= check of the version to avoid unnecessary
      commits on every new release
    
    Change-Id: I5a49c9571bba603af7873222e5383ec0494619e4

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2009 The Android Open Source Project
 # Copyright (c) 2011, The Linux Foundation. All rights reserved.
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,19 +19,19 @@ import common
 import re
 
 def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
+  AddModemAssertion(info, info.input_zip)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
+  AddModemAssertion(info, info.target_zip)
   return
 
-def AddTrustZoneAssertion(info, input_zip):
+def AddModemAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(\S+)', android_info)
   if m:
     versions = m.group(1).split('|')
     if len(versions) and '*' not in versions:
-      cmd = 'assert(xiaomi.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+      cmd = 'assert(xiaomi.verify_modem(' + ','.join(['"%s"' % modem for modem in versions]) + ') == "1");'
       info.script.AppendExtra(cmd)
   return
