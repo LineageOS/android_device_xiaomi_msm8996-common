2a36d327d117a4ccb4bcb7dc363ad7b37abdb982
Bruno Martins <bgcngm@gmail.com>
2017-05-08 23:59:16 +0100

    msm8996-common: Add lithium to build guard
    Change-Id: I3a462017223741fc5156ab3e68d97946c5c834d6

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gemini natrium,$(TARGET_DEVICE)),)
+ifneq ($(filter gemini lithium natrium,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
