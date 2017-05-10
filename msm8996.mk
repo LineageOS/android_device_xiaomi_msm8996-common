10f909f2dc4f7d8a0e250eebbb58f3ec76c7d9c3
Bruno Martins <bgcngm@gmail.com>
2017-05-10 13:13:47 +0000

    msm8996-common: Kill Gello
    Change-Id: Id89360b38da4cfbb3afbe742764354d4ae19e17c

--- msm8996.mk
+++ msm8996.mk
@@ -112,10 +112,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
