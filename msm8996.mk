d6dbf9ceef94f22eafc0776a595d25c2ff8171ae
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-05-28 01:20:08 +0800

    msm8996-common: Build the OSS camera HAL
     * Drop prebuilt camera HAL and face recognition blobs.
    
     * Disable camera HAL1 support because it's broken while switching
       camera modes.
    
     * Remove no longer needed flags and system props.
    
    Change-Id: I581a1e91ec17cfce9762e17060cd892605d6a163

--- msm8996.mk
+++ msm8996.mk
@@ -135,6 +135,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    camera.msm8996 \
+    libmm-qcamera \
     Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
