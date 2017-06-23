ff15bb6264ca2dd2588f888562562c4b2d1cac7c
Luca Stefani <luca020400@lineageos.org>
2017-06-23 18:18:11 +0200

    msm8996-common: Build readfem
    Change-Id: I6ac3a18fdf9795eefc6dd037ddb50f6b899b39f8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,14 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 # Common config scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.cnss.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.cnss.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.panel.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
