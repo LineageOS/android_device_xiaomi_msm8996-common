c119400dd5e74f8feea1a8bf91d02c01d2812a90
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-07-08 23:42:51 +0800

    msm8996-common: camera: Fixes to OSS camera hal to build in LineageOS tree
    * Fix inlude paths
    
    Change-Id: I9ce7744a23c059cef5221a506173f88099a30bb7

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,27 +1,7 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8909
-MM_V4L2_DRIVER_LIST += msm8952
-MM_V4L2_DRIVER_LIST += msm8996
-MM_V4L2_DRIVER_LIST += msm8992
-MM_V4L2_DRIVER_LIST += msm8937
-MM_V4L2_DRIVER_LIST += msm8953
-MM_V4L2_DRIVER_LIST += msmcobalt
-MM_V4L2_DRIVER_LIST += msmfalcon
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
-  endif
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+include $(call all-subdir-makefiles)
+endif
+
 endif
