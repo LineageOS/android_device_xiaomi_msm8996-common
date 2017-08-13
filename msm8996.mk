ed52e2f0e5593381b7316f0a7a806c27570e7190
dianlujitao <dianlujitao@lineageos.org>
2017-08-13 22:36:42 +0800

    msm8996-common: Kang marlin thermal config file
    Change-Id: I6a5d4c8327cdfdcb62647421b8506dcbd303a204

--- msm8996.mk
+++ msm8996.mk
@@ -384,6 +384,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
