305316e99f1f22c0c63c522829ed33f557a483a3
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    msm8996-common: init: Update for O
    Change-Id: I635dc8d27f92f9a2fa83da62c2f32e48bd2c7680

--- init/Android.mk
+++ init/Android.mk
@@ -22,5 +22,7 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_msm8996.cpp
 LOCAL_MODULE := libinit_msm8996
+LOCAL_STATIC_LIBRARIES := \
+    libbase
 
 include $(BUILD_STATIC_LIBRARY)
