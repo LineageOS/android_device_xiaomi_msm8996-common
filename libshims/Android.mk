2e8d53683a2b98b55aee9c27e5bdad6ffca27044
Luca Stefani <luca020400@lineageos.org>
2017-08-19 12:10:27 +0200

    gps: Add get_process_name shim
    Change-Id: I10ce654b14866d41610c7e4098df9691b983d515

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -24,3 +24,12 @@ LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := get_process_name.c
+
+LOCAL_MODULE := libshims_get_process_name
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
