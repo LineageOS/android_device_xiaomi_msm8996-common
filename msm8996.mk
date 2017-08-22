661977fe14f404cb47aa4be67a6b66762adb1f7b
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:12 +0200

    msm8996-common: Build GNSS HIDL HAL
    Change-Id: I4241ab6369fc04814f0b4e6d1684ababb483f48a

--- msm8996.mk
+++ msm8996.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8996 \
