b326865b4674eca32c5c3f6ba8e568493535d103
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    msm8996-common: Build vibrator HIDL HAL
    Change-Id: I3353060afd4f7b63ebd94a3a46d40cb7e651d8b5

--- msm8996.mk
+++ msm8996.mk
@@ -289,6 +289,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshims_ims
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
