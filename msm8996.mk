e1ae716db283309b1d0fef030669707dc8cf8ce9
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    msm8996-common: Add HIDL manifest
     * This defines the interfaces for graphics and wifi
    
    Change-Id: I2c3aece8ad13613ec5b83496aa23d26ef238f45d

--- msm8996.mk
+++ msm8996.mk
@@ -188,6 +188,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
 
+# HIDL manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
