7d01e25bf2d7a75a8366dc304f66bb53a17075a8
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    msm8996-common: Build Audio HIDL HAL interfaces
    Change-Id: Ide624d3008739b68a1b4cac347237e72cd4b4b20

--- msm8996.mk
+++ msm8996.mk
@@ -84,6 +84,10 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8996 \
