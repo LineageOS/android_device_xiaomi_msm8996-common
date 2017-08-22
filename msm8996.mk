25d9a5ad6824d6e5b2919819b7ee56b38e8c0331
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    msm8996-common: Build Display HIDL HAL interfaces
     * Enable binderized Gralloc HAL along with it
    
    Change-Id: I61c34a531e3131209a08b5b4670e1f1f8fbb435d

--- msm8996.mk
+++ msm8996.mk
@@ -145,6 +145,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     copybit.msm8996 \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
