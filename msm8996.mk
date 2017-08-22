86364cf359399c919a2216072177cd51a19a8bd6
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:33 +0200

    msm8996-common: Build USB HIDL HAL
    Change-Id: I993d76c70f50ae6f8f6c3f468d9a363fad6711d5

--- msm8996.mk
+++ msm8996.mk
@@ -294,6 +294,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshims_ims
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
