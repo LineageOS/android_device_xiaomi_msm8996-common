2146803d0966d2724d7b27b31c3e7206a3b35b12
Luca Stefani <luca020400@lineageos.org>
2017-08-24 19:22:46 +0200

    msm8996-common: Add rild socket shim
    Change-Id: Ia5aa2cf867678045215625f6297708db6f49fdaa

--- msm8996.mk
+++ msm8996.mk
@@ -195,6 +195,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
 
+# IMS
+PRODUCT_PACKAGES += \
+    libshims_ims
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
@@ -282,7 +286,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     librmnetctl \
-    libxml2
+    libxml2 \
+    libshims_rild_socket
 
 # Sensors
 PRODUCT_PACKAGES += \
@@ -291,10 +296,6 @@ PRODUCT_PACKAGES += \
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
-
-# Shims
-PRODUCT_PACKAGES += \
-    libshims_ims
 
 # USB
 PRODUCT_PACKAGES += \
