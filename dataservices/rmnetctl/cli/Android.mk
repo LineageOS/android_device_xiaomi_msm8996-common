2df3337f33f3da89815662dee5c9c11917aecb6b
Luca Stefani <luca020400@lineageos.org>
2017-08-24 22:24:29 +0200

    msm8996-common: Import marlin dataservices
    Change-Id: I9d2e47585ac93bbdf28e275e9ec0c669b9f8be3e

---
+++ dataservices/rmnetctl/cli/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := rmnetcli.c
+LOCAL_CFLAGS := -Wall -Werror
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../inc
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../src
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+
+LOCAL_MODULE := rmnetcli
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := qcom
+LOCAL_PROPRIETARY_MODULE := true
+
+LOCAL_SHARED_LIBRARIES := librmnetctl
+include $(BUILD_EXECUTABLE)
