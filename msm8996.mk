b54f876dc39bab9fd26d2b798251efb4e69d29c5
Luca Stefani <luca020400@lineageos.org>
2017-08-26 13:58:29 +0200

    msm8996-common: Switch to HIDL multihal
    Change-Id: I181b825e57b61f78b40f9db5ef6ce1703908a3a8

--- msm8996.mk
+++ msm8996.mk
@@ -292,8 +292,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    sensors.msm8996
+    android.hardware.sensors@1.0-impl
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
