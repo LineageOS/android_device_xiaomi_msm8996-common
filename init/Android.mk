42557433293fc772e98f8fbc3bebf55e12fd9c10
Bruno Martins <bgcngm@gmail.com>
2017-09-09 00:52:49 +0100

    msm8996-common: init: Use core init function to read from files
    Change-Id: I3fd23490bcdfa4097dad73161ad226337e93cd18

--- init/Android.mk
+++ init/Android.mk
@@ -18,7 +18,9 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_msm8996.cpp
 LOCAL_MODULE := libinit_msm8996
