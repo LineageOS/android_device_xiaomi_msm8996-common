fca76dd62aba5b0d3b7f3f8bf6bea817d1d58657
Luca Stefani <luca020400@lineageos.org>
2017-09-30 15:23:37 +0200

    msm8996-common: Update to MIUI 7.9.22 Global dev blobs and configs
    * Add and enable lowi
    * Update media codecs performance
    * Update WCNSS config
    
    Change-Id: If4d9bb9f4279f66df0e01ccdbca3c45267cb677b

--- msm8996.mk
+++ msm8996.mk
@@ -187,6 +187,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
