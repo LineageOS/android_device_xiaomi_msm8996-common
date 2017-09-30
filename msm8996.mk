8ee2351125979e3d60942d41d33d123408f22871
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 19:34:50 -0400

    msm8996-common: Build netd HAL and netutils wrapper for netmgrd
    Change-Id: I93453934a2f3e06296eb4f7a62941b9b6abca703

--- msm8996.mk
+++ msm8996.mk
@@ -246,6 +246,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.0 \
+    netutils-wrapper-1.0
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
