92d05f8d33096caeef694bb3dc1085ff4c29657a
Bruno Martins <bgcngm@gmail.com>
2017-10-05 00:33:30 +0100

    msm8996-common: ConfigPanel: Apply LineageOS rebrand
    Change-Id: I33178e6bc605014033c498ad20c1224e8f4f007d

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -12,7 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
-    org.cyanogenmod.platform.internal
+    org.lineageos.platform.internal
 
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
