7140bda75be6341391724f58f08558f27416aea8
Bruno Martins <bgcngm@gmail.com>
2017-10-05 00:33:30 +0100

    msm8996-common: ConfigPanel: Apply LineageOS rebrand
    Change-Id: I33178e6bc605014033c498ad20c1224e8f4f007d

--- configpanel/src/org/lineageos/settings/device/Utils.java
+++ configpanel/src/org/lineageos/settings/device/Utils.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.device;
+package org.lineageos.settings.device;
 
 import android.content.Context;
 import android.content.Intent;
