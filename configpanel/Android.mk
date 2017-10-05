42af9a811fe23b5a9e1d581c3bad635cec82e03e
Bruno Martins <bgcngm@gmail.com>
2017-10-05 23:08:09 +0100

    msm8996-common: ConfigPanel: Re-integrate into Settings
     * Apply the default Oreo theme and inherit the layouts
       from Google for the Settings app, in order to keep
       UI consistency.
    
     * Get rid of SettingsDrawerActivity as it no longer fits in.
    
     * Use the new information architecture for the Settings app
       and integrate into System category (this will be temporary,
       only until a whole new category to aggregate all Lineage
       button-dedicated features is created).
    
    Change-Id: Ic2e9768552751a59e49ec00576b202e8223268e7

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -8,10 +8,12 @@ LOCAL_PACKAGE_NAME := ConfigPanel
 LOCAL_REQUIRED_MODULES := XiaomiPocketMode
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v14-preference \
+    android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-preference \
     android-support-v7-recyclerview \
+    android-support-v13 \
+    android-support-v14-preference \
     org.lineageos.platform.internal
 
 LOCAL_AAPT_FLAGS := \
@@ -33,7 +35,5 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
-
-include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
