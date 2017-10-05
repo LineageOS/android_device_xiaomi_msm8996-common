02219239609b48e28d46b3b3e7b4655e33affac5
Bruno Martins <bgcngm@gmail.com>
2017-10-05 23:08:09 +0100

    msm8996-common: ConfigPanel: Re-integrate into Settings
     * Use the new information architecture for the Settings app
       and integrate into homepage (this will most likely be temporary,
       until a whole new category to aggregate all Lineage button-dedicated
       features is created).
    
     * Apply the default Oreo theme.
    
    Change-Id: Ic2e9768552751a59e49ec00576b202e8223268e7

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -34,6 +34,4 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
-include frameworks/base/packages/SettingsLib/common.mk
-
 include $(BUILD_PACKAGE)
