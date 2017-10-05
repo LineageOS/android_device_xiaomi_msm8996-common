6a575c73a28d2c452f9f15b8394215cc919dd819
Bruno Martins <bgcngm@gmail.com>
2017-10-05 23:33:44 +0100

    msm8996-common: ConfigPanel: Restore back button to the action bar
     * This restores the relevant code that was removed in commit c243b72bf0e5bd460167b7bb7d2f4286a5e4eea8
    
    Change-Id: I69afd5f8ef4c59f215a201bd76acc3b2015c0a73

--- configpanel/src/org/lineageos/settings/device/ButtonSettingsFragment.java
+++ configpanel/src/org/lineageos/settings/device/ButtonSettingsFragment.java
@@ -17,6 +17,7 @@
 
 package org.lineageos.settings.device;
 
+import android.app.ActionBar;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v14.preference.PreferenceFragment;
@@ -38,6 +39,8 @@ public class ButtonSettingsFragment extends PreferenceFragment
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.button_panel);
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -118,6 +121,15 @@ public class ButtonSettingsFragment extends PreferenceFragment
                 !FileUtils.fileExists(Constants.FP_WAKEUP_NODE)) {
             getPreferenceScreen().removePreference(fingerprintCategory);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            getActivity().onBackPressed();
+            return true;
+        }
+        return false;
     }
 
     private void updatePreferencesBasedOnDependencies() {
