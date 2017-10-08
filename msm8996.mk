b61a50b6779c3d688af7c498fcef8a0e743af694
Bruno Martins <bgcngm@gmail.com>
2017-10-08 11:39:26 +0100

    msm8996-common: Switch to custom vibrator HAL service
     * Using LineageOS' custom vibrator HAL service allows to ditch
       old CMHW intensity implementation.
    
    Change-Id: I424b5767fe62b611b20f90f6bef15f9fb85a19ff

--- msm8996.mk
+++ msm8996.mk
@@ -303,7 +303,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.lineage
 
 # WiFi
 PRODUCT_PACKAGES += \
