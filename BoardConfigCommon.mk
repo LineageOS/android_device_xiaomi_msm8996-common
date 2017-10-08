0d1b37200011cae7f8762d4bf40829e094a06e36
Bruno Martins <bgcngm@gmail.com>
2017-10-08 11:39:26 +0100

    msm8996-common: Switch to custom vibrator HAL service
     * Using LineageOS' custom vibrator HAL service allows to ditch
       old CMHW intensity implementation.
    
    Change-Id: I424b5767fe62b611b20f90f6bef15f9fb85a19ff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,9 +111,7 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
     $(VENDOR_PATH)/cmhw
-BOARD_USES_CYANOGEN_HARDWARE := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
