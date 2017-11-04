1c559db46ef1a0286d87a6756cc6b6b0fd90f53b
dianlujitao <dianlujitao@lineageos.org>
2017-11-04 14:07:54 +0800

    msm8996-common: Remove ipacm/ipanat
     * Disabled by Qualcomm in O
    
    Change-Id: I063355c2c0bf188bcbafca09f2ea5569a0e5d2f2

--- msm8996.mk
+++ msm8996.mk
@@ -206,11 +206,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8996-tasha-mtp-snd-card_Button_Jack.kl:system/usr/keylayout/msm8996-tasha-mtp-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
 
-# IPA Manager
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
-
 # IPC router config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
