862ebf715c91ca6df1148367546fcb85263d70d4
LuK1337 <priv.luk@gmail.com>
2017-12-21 16:06:59 +0100

    msm8996-common: sepolicy: Initial SELinux O updates
    Change-Id: Iae783fda17c9e747a824bce8b12a39971faa0bdc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
