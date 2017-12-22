f4a2868dcf8075959de100b99e6202a6116505fe
Bruno Martins <bgcngm@gmail.com>
2017-12-22 17:34:58 +0000

    msm8996-common: Remove old AANC tunning
     * This file was turned into speaker mixer controls for RAS test device
       and is no longer used for AANC tunning [1]. Useless for us, so remove it.
    
       [1] https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio/commit/?h=LA.UM.6.5.r1-04300-8x96.0&id=744d930bd8d0f0a7b2aa6b029313caf2f00f647d
    
    Change-Id: Ib32ff3ad9c9c8bb69cd4f0c3b7053650bbf9ca1a

--- msm8996.mk
+++ msm8996.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
