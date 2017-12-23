75888816acb2c4114b1e32d26474e06e1a2c9b1f
Bruno Martins <bgcngm@gmail.com>
2017-12-23 19:42:59 +0000

    msm8996-common: Remove Broadcast Radio HAL impl
     * There is no support for FM radio on the Xiaomi 8996 family
    
    Change-Id: I64f9f0f3fe403d4bd863ef214af24d40f312f9ba

--- msm8996.mk
+++ msm8996.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
