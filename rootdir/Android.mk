6a794a40621607bfeeb6e06461d4ffabdeee89b4
LuK1337 <priv.luk@gmail.com>
2017-12-23 12:15:33 +0100

    msm8996-common: Unbreak init.qcom.sh
    * Move it to executables directory
    * Fix it's SELinux domain policies
    
    Change-Id: I742c496541256acafb3065dd9ea7e62b76732b0a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,6 +10,14 @@ LOCAL_SRC_FILES    := etc/init.panel.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # Common init scripts
 
 include $(CLEAR_VARS)
@@ -25,14 +33,6 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
