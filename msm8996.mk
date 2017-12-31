8f9bcc1789bad44033135b91deebed411e678da2
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    msm8996-common: Build QTI HIDL power 1.0 HAL
     * Despite all Xiaomi 8996 targets have QCACLD2 WLAN card
       and the node that should return WiFi stats is available,
       it seems that the firmware doesn't fully supports it,
       causing freezes when trying to access the node.
       Just go with the HAL that provides no support for this
       feature.
    
    Change-Id: I929a2d3aedf3bfdca962dc34720019d83c7134e1

--- msm8996.mk
+++ msm8996.mk
@@ -281,8 +281,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8996
+    android.hardware.power@1.0-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
