69e5f184dee22a8fb31a813892de1ddd550ee8fc
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    msm8996-common: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- msm8996.mk
+++ msm8996.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
