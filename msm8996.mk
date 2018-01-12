097bb44ae52683a34164915b383b4c39af0e6b1f
Bruno Martins <bgcngm@gmail.com>
2018-01-12 22:03:34 +0000

    msm8996-common: Build Lineage vibrator HAL
    Change-Id: Id60fb36cbae9f4a6dfb0cd926e7c05b798c5699d

--- msm8996.mk
+++ msm8996.mk
@@ -322,7 +322,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.lineage
 
 # WiFi
 PRODUCT_PACKAGES += \
