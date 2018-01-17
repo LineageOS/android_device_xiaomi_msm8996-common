a19c997e66b823a5391221e14f56ce653289d396
LuK1337 <priv.luk@gmail.com>
2018-01-17 19:13:33 +0100

    Revert "msm8996-common: Build Lineage vibrator HAL"
    This reverts commit 097bb44ae52683a34164915b383b4c39af0e6b1f.
    
    Change-Id: Iafa36f4d00717b86144d88170e9bdae697ef4ff9

--- msm8996.mk
+++ msm8996.mk
@@ -356,7 +356,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-impl
 
 # WiFi
 PRODUCT_PACKAGES += \
