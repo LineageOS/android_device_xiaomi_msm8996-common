efbc12f388144c921b6494f8907d07a8ee7910c3
Bruno Martins <bgcngm@gmail.com>
2018-01-19 15:42:00 +0000

    msm8996-common: Convert lights HAL into a native binderized HAL
    Change-Id: If41458e22b9a67a5c2a415571723917df1904aa1

--- msm8996.mk
+++ msm8996.mk
@@ -242,8 +242,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8996
+    android.hardware.light@2.0-service.xiaomi_8996
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
