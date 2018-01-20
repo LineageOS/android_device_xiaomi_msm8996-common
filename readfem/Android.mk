178c973f54819a00b0972fbb1735c5e294e35fcd
LuK1337 <priv.luk@gmail.com>
2018-01-20 16:35:15 +0100

    msm8996-common: Move readmac and readfem to /vendor
    Change-Id: Ifea0f1c649717a5b1ea50449e6447e40cf00791a

--- readfem/Android.mk
+++ readfem/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE           := readfem
-LOCAL_MODULE_TAGS      := optional
-LOCAL_SRC_FILES        := xiaomi_readfem.c
-LOCAL_CFLAGS           += -Wall -Werror
-LOCAL_SHARED_LIBRARIES := libcutils liblog
+LOCAL_MODULE             := readfem
+LOCAL_MODULE_TAGS        := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SRC_FILES          := xiaomi_readfem.c
+LOCAL_CFLAGS             += -Wall -Werror
+LOCAL_SHARED_LIBRARIES   := libcutils liblog
 include $(BUILD_EXECUTABLE)
 endif
--- readmac/Android.mk
+++ readmac/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
-LOCAL_MODULE           := readmac
-LOCAL_MODULE_TAGS      := optional
-LOCAL_SRC_FILES        := xiaomi_readmac.c
-LOCAL_CFLAGS           += -Wall -Werror
-LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE             := readmac
+LOCAL_MODULE_TAGS        := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_SRC_FILES          := xiaomi_readmac.c
+LOCAL_CFLAGS             += -Wall -Werror
+LOCAL_SHARED_LIBRARIES   := liblog
 include $(BUILD_EXECUTABLE)
 endif
