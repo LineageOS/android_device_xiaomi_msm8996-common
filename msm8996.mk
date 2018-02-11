d8a96c4f694c2527ff6f9172e8e0ebf1c4f23095
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-11 11:40:16 +0100

    msm8996-common: Binderize sensors
    Change-Id: I3bf7daa30f7a3d876df8f967212f897eb154bd5f

--- msm8996.mk
+++ msm8996.mk
@@ -345,7 +345,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 # Telephony
 PRODUCT_PACKAGES += \
