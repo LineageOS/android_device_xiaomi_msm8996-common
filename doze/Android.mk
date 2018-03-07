78a606dd0e576038dfa237af5f5a9be66dad1993
Bruno Martins <bgcngm@gmail.com>
2018-03-07 10:56:42 +0000

    msm8996-common: Doze: Install into vendor
    Change-Id: Ideb432998aed70d9c7d24f8b60d9a28f026d9c37

--- doze/Android.mk
+++ doze/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_USE_AAPT2 := true
 
