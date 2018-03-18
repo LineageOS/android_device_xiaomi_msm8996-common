ce11e8efed0c4ec4e49cb5c4e0344d0ec231397e
Bruno Martins <bgcngm@gmail.com>
2018-03-18 21:20:20 +0000

    msm8996-common: Remove unexisting HAL services
    Change-Id: I26da013dfa359ec8f184a20bc881558be0144b4a

--- msm8996.mk
+++ msm8996.mk
@@ -93,9 +93,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
