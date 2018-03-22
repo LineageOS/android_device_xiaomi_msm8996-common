ec2f3fd382867a89a7dfd755517e6309ab4a9f43
Bruno Martins <bgcngm@gmail.com>
2018-03-22 14:04:39 +0000

    msm8996-common: PocketMode: Include proper SDK lib for platform apps
    Change-Id: Ib61f93704c6411a4cb1f91d9e8693a1775934037

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -10,7 +10,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform
+    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
