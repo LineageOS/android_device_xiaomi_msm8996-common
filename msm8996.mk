62399949dd493648466b92dbe9137b3caf96b3d5
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-04-05 16:44:49 +0200

    msm8996-common: Decommonize Snap
    Change-Id: I026189567f14ba879bf9a7054c5c27e14921bfe1
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -135,7 +135,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8996 \
     libmm-qcamera \
-    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
