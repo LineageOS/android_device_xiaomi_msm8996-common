e803d38970fc11ee9b62ae901c9ab27ff52aec21
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:13:39 +0100

    msm8996-common: Switch to HIDL ANT+ HAL
    Change-Id: I0e0252b6e2c7e3e7da1e8508f010b7e6c2bfd128

--- msm8996.mk
+++ msm8996.mk
@@ -86,8 +86,7 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 # Audio
 PRODUCT_PACKAGES += \
