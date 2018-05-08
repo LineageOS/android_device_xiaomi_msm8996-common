5652434915cf4f91ce516455ef6723d4e5d90455
Logan Chien <loganchien@google.com>
2018-05-08 20:14:03 +0800

    msm8996-common: Migrate to versioned VNDK layout
    Bug: 78605339
    Test: aosp_sailfish-userdebug builds and boots
    Test: sailfish + GSI boots
    Change-Id: I929ad41a834589a37c67515b65c4973db033c4fd
    Merged-In: I929ad41a834589a37c67515b65c4973db033c4fd
    (cherry picked from commit ae40fd0df736950406d6bcc909f0006be9733dc0)

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -16,6 +16,8 @@ VNDK_SP_LIBRARIES := \
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
 
+vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
+
 define define-vndk-sp-lib
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.vndk-sp-gen
@@ -26,7 +28,7 @@ LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := vndk-sp$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
 include $$(BUILD_PREBUILT)
 
 ifneq ($$(TARGET_2ND_ARCH),)
@@ -40,7 +42,7 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := vndk-sp$(if $(filter $1,$(install_in_hw_dir)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $(vndk_sp_dir)$(if $(filter $1,$(install_in_hw_dir)),/hw)
 include $$(BUILD_PREBUILT)
 endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
