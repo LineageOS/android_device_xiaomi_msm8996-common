8b4b95dafaab54a5472809e86bb1673d946f75bd
Bruno Martins <bgcngm@gmail.com>
2018-05-11 23:32:31 +0100

    msm8996-common: Restore audio tuning mixer control
     * After all, this still seems to be needed for ACDB MCS init,
       as seen bellow. Just let ACDB loader be happy.
    
       Without the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         E MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
         E ACDB-MCS: acdb_mcs_init: MCS routing control initialization failed.
    
       With the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         D         : [ACPH]->MCS RTC service registered with ACPH
    
    Change-Id: I6e4b1fe27547a354eaa4132aba8e3df04d6184b3

--- msm8996.mk
+++ msm8996.mk
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
