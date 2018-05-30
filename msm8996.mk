20fd9de77a115ba711aae505ad6c7e104d501b4a
Bruno Martins <bgcngm@gmail.com>
2018-05-30 17:31:36 +0100

    msm8996-common: Remove QC location and other unused GPS blobs
    Change-Id: I2e76e45160f028464eca34bb78da028f7d8dba8c

--- msm8996.mk
+++ msm8996.mk
@@ -207,7 +207,6 @@ PRODUCT_PACKAGES += \
     libgnsspps
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
     $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
