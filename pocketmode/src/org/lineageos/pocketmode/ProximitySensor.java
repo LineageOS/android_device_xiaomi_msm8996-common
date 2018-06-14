394ae470976f456bf45ad983217de67ea43d580f
ezio84 <brabus84@gmail.com>
2018-06-14 15:00:19 +0200

    msm8996-common: pocketmode: use ExecutorService for listener registration
    Replicate what Google did for SystemUI in this commit.
    aosp-mirror/platform_frameworks_base@fabc743
    
    Registering a sensor seems to be an expensive operation,
    and we do it on each screen-on event, so moving it to
    an asynchronous task looks like a good idea anyway.
    
    By moving all non-essential binder calls of the main thread or to the
    next frame, we bring this down to 5ms, such that window animation
    and Keyguard animation starts about at the same time.
    
    The interesting part about the ExecutorService:
    "Memory consistency effects: Actions in a thread prior to the submission
    of a Runnable or Callable task to an ExecutorService happen-before any
    actions taken by that task, which in turn happen-before the result is
    retrieved via Future.get()."
    (from https://developer.android.com/reference/java/util/concurrent/ExecutorService)
    
    Change-Id: I453e64a7c4d40b6f49fb97f955a6a166ae5d58d5

--- pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
+++ pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -26,6 +26,10 @@ import android.util.Log;
 
 import org.lineageos.internal.util.FileUtils;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ProximitySensor implements SensorEventListener {
 
     private static final boolean DEBUG = false;
@@ -36,11 +40,17 @@ public class ProximitySensor implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context mContext;
+    private ExecutorService mExecutorService;
 
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mExecutorService = Executors.newSingleThreadExecutor();
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
     }
 
     @Override
@@ -58,12 +68,16 @@ public class ProximitySensor implements SensorEventListener {
 
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
-        mSensorManager.registerListener(this, mSensor,
-                SensorManager.SENSOR_DELAY_NORMAL);
+        submit(() -> {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        });
     }
 
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
-        mSensorManager.unregisterListener(this, mSensor);
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
     }
 }
