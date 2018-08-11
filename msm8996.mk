f2bdd3ed30dfc0081f33162ee9558aaba31fae39
LuK1337 <priv.luk@gmail.com>
2018-08-11 10:51:05 +0200

    msm8996-common: Copy and use android.hardware.gnss@1.0.so from v27
    Since we are rolling back to O GPS stack, we need to do that
    otherwise we end up with missing symbols in our GPS stack.
    
    Change-Id: Ib5df8bc6181158505bbfa7197945f57390013426

--- msm8996.mk
+++ msm8996.mk
@@ -387,6 +387,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
