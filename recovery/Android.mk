3e178ee9e9416048e931c0f421d2eb83640f971f
LuK1337 <priv.luk@gmail.com>
2018-08-12 20:51:09 +0200

    msm8996-common: Fix build of librecovery_updater_xiaomi
    Change-Id: I308fca4987b049266ebaae04de8b3e0efbf836ba

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_xiaomi
 LOCAL_MODULE_TAGS := eng
