f85729c156eca31c621a00d48a59f65a9a11f793
LuK1337 <priv.luk@gmail.com>
2018-08-12 11:14:29 +0200

    msm8996-common: Disable our and QCOM sepolicy
    Change-Id: Id6d617ead095ef4a0d37e0d8ee1996cd7f0b10bc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,9 +184,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_msm8996
