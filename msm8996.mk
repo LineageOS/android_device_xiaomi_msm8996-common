f006bbda3c4d4c3292b985cd672e1d1e294d997f
LuK1337 <priv.luk@gmail.com>
2018-08-17 01:22:47 +0200

    msm8996-common: Add HandleUsingDestroyedMutex shim for qcom camera server
    Change-Id: Ie78dd999f7cff977ee3c1e339260f49ddcfee062

--- msm8996.mk
+++ msm8996.mk
@@ -134,7 +134,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
-    libmm-qcamera
+    libmm-qcamera \
+    libshims_camera
 
 # CNE
 PRODUCT_PACKAGES += \
