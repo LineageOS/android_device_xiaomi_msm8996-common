6d5232192450ad32934e8dadc31101b53f91f137
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    msm8996-common: Switch to common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- msm8996.mk
+++ msm8996.mk
@@ -353,6 +353,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
