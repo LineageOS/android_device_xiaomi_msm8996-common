6eb3d4703bd75f3af814a286b0855e8e301dd112
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    msm8996-common: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- msm8996.mk
+++ msm8996.mk
@@ -346,6 +346,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
