383272e1d9075bb152f8c3d5e9b47159d3ed4d5b
Bruno Martins <bgcngm@gmail.com>
2018-11-12 16:36:38 +0000

    msm8996-common: Update GPS HAL to P
     * Tag: LA.UM.7.5.r1-03100-8x96.0
    
    Change-Id: I2cfa7eeb03f19c9b6fb9be283146d0ef3f3dbe99

--- gps/Android.mk
+++ gps/Android.mk
@@ -15,4 +15,7 @@
 # limitations under the License.
 #
 
-include $(call first-makefiles-under,$(call my-dir))
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/build/target_specific_features.mk
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
