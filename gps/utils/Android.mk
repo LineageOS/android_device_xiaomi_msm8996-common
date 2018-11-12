d79efe4f34ec19195313a64e8dc9afd03886ffd8
Bruno Martins <bgcngm@gmail.com>
2018-11-12 16:36:38 +0000

    msm8996-common: Update GPS HAL to P
     * Tag: LA.UM.7.5.r1-03100-8x96.0
    
    Change-Id: I2cfa7eeb03f19c9b6fb9be283146d0ef3f3dbe99

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -2,12 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+
 ## Libs
 LOCAL_SHARED_LIBRARIES := \
     libutils \
     libcutils \
-    liblog \
-    libloc_pla
+    liblog
 
 LOCAL_SRC_FILES += \
     loc_log.cpp \
@@ -15,13 +15,13 @@ LOCAL_SRC_FILES += \
     msg_q.c \
     linked_list.c \
     loc_target.cpp \
-    platform_lib_abstractions/elapsed_millis_since_boot.cpp \
     LocHeap.cpp \
     LocTimer.cpp \
     LocThread.cpp \
     MsgTask.cpp \
     loc_misc_utils.cpp \
-    loc_nmea.cpp
+    loc_nmea.cpp \
+    LocIpc.cpp
 
 # Flag -std=c++11 is not accepted by compiler when LOCAL_CLANG is set to true
 LOCAL_CFLAGS += \
@@ -36,13 +36,15 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
 LOCAL_HEADER_LIBRARIES := \
+    libutils_headers \
     libloc_pla_headers \
     liblocation_api_headers
 
 LOCAL_MODULE := libgps.utils
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRELINK_MODULE := false
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
 
@@ -52,5 +54,3 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libgps.utils_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 include $(BUILD_HEADER_LIBRARY)
-
-include $(addsuffix /Android.mk, $(addprefix $(LOCAL_PATH)/, platform_lib_abstractions))
