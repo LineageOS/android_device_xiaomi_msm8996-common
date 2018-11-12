7aa5e9ba7f7d1a0b5893c1c172fcfbdae305362c
Bruno Martins <bgcngm@gmail.com>
2018-11-12 16:36:38 +0000

    msm8996-common: Update GPS HAL to P
     * Tag: LA.UM.7.5.r1-02500-8x96.0
    
    Change-Id: I2cfa7eeb03f19c9b6fb9be283146d0ef3f3dbe99

--- gps/pla/android/loc_pla.h
+++ gps/pla/android/loc_pla.h
@@ -26,16 +26,33 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "loc_stub_android_runtime.h"
+#ifndef __LOC_PLA__
+#define __LOC_PLA__
 
-namespace android {
+#ifdef __cplusplus
+#include <utils/SystemClock.h>
+#define uptimeMillis android::uptimeMillis
+#endif
 
-pthread_t AndroidRuntime::createJavaThread(const char* /*name*/,
-    void (*start)(void *), void* arg)
-{
-    pthread_t threadId = 0;
-    pthread_create(&threadId, NULL, (void *(*)(void*))start, arg);
-    return threadId;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <cutils/properties.h>
+#include <cutils/threads.h>
+#include <cutils/sched_policy.h>
+
+#define LOC_PATH_GPS_CONF_STR      "/vendor/etc/gps.conf"
+#define LOC_PATH_IZAT_CONF_STR     "/vendor/etc/izat.conf"
+#define LOC_PATH_FLP_CONF_STR      "/vendor/etc/flp.conf"
+#define LOC_PATH_LOWI_CONF_STR     "/vendor/etc/lowi.conf"
+#define LOC_PATH_SAP_CONF_STR      "/vendor/etc/sap.conf"
+#define LOC_PATH_APDR_CONF_STR     "/vendor/etc/apdr.conf"
+#define LOC_PATH_XTWIFI_CONF_STR   "/vendor/etc/xtwifi.conf"
+#define LOC_PATH_QUIPC_CONF_STR    "/vendor/etc/quipc.conf"
+
+#ifdef __cplusplus
 }
+#endif /*__cplusplus */
 
-}
+#endif /* __LOC_PLA__ */
