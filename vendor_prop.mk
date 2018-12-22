c4ef3d743132fbfddda4e2e497feea9de59f0f0e
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    msm8996-common: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,6 +112,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.enc.disable.pq=true \
     vidc.enc.dcvs.extra-buff-count=2
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.port=I2C \
