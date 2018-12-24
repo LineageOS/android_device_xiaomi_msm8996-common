54b88b1689eac48e8136f4e06448bdaf2937aec8
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    msm8996-common: Add QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -95,7 +95,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
-    persist.sys.wfd.virtual=0 \
     ro.opengles.version=196610 \
     ro.sf.lcd_density=480 \
     sdm.debug.disable_rotator_split=1 \
