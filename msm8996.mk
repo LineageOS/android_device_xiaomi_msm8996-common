39b7f0aac5cde7f2018115665d95373c34682d1d
Bruno Martins <bgcngm@gmail.com>
2019-01-15 00:24:17 +0000

    msm8996-common: Replace KeyDisabler class with Touch HAL
    Change-Id: I7b87ae0ad834ba02a78696afe393d9d4f8920fbd

--- msm8996.mk
+++ msm8996.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -355,6 +355,10 @@ PRODUCT_BOOT_JARS += \
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
+
+# Touch HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi_8996
 
 # Trust HAL
 PRODUCT_PACKAGES += \
