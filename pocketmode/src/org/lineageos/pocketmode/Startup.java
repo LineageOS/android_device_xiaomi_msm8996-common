2e6e142064ebffd6acb8c4764026701f0084cb1b
LuK1337 <priv.luk@gmail.com>
2019-07-07 11:07:27 +0200

    msm8996-common: pocketmode: Listen for android.intent.action.BOOT_COMPLETED
    Change-Id: I2436a38bddccb908acf94d916d0220918e8b6a0c

--- pocketmode/src/org/lineageos/pocketmode/Startup.java
+++ pocketmode/src/org/lineageos/pocketmode/Startup.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2016 The CyanogenMod Project
- *               2017 The LineageOS Project
+ *               2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,11 +29,8 @@ public class Startup extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        final String action = intent.getAction();
-        if (lineageos.content.Intent.ACTION_INITIALIZE_LINEAGE_HARDWARE.equals(action)) {
-            Log.d(TAG, "Starting");
-            context.startServiceAsUser(new Intent(context, PocketModeService.class),
-                    UserHandle.CURRENT);
-        }
+        Log.d(TAG, "Starting");
+        context.startServiceAsUser(new Intent(context, PocketModeService.class),
+              UserHandle.CURRENT);
     }
 }
