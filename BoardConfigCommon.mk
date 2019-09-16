28b244fc11617bee616cbed1e19701956a7f51d5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 19:02:36 +0000

    msm8996-common: Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//device/oneplus/oneplus3/init/libinit_oneplus3"
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
-TARGET_INIT_VENDOR_LIB := libinit.xiaomi_8996
+TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit.xiaomi_8996
 TARGET_RECOVERY_DEVICE_MODULES := libinit.xiaomi_8996
 
 # Wifi
