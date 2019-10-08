234a54211796d173cd92cdabc63545b226362871
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:35:22 +0300

    msm8996-common: Build vendor variants of tinymix/tinyplay
    * Avoid SELinux neverallows, vendor services should use vendor tools
    
    Change-Id: I75f78f1b4990673d3685d62a72fa91c0fe65e630

--- msm8996.mk
+++ msm8996.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    tinymix
+    tinymix.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
