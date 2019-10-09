89de21742c88df2e14f17e732780cac6eda2f050
dianlujitao <dianlujitao@lineageos.org>
2019-10-09 09:26:39 +0800

    Revert "msm8996-common: Create system copy of ims-ext-common and add to classpath"
    No longer needed with Q ims.apk
    
    This reverts commit 8187364cb4a34ca889915160180665e350981477.
    
    Change-Id: I4e2429e78204ed5a65cc1a2659f0065b78e30865

--- msm8996.mk
+++ msm8996.mk
@@ -365,7 +365,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    ims-ext-common_system \
+    ims-ext-common \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
@@ -373,7 +373,6 @@ PRODUCT_PACKAGES += \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # Tethering
