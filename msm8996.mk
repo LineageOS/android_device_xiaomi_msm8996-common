87b70a5ee3ea174bf18dc4bf8083e18639bcf18c
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    msm8996-common: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- msm8996.mk
+++ msm8996.mk
@@ -326,13 +326,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/permissions/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
