c9e747da3a583061e9c4c2105f55eb3c093dd7bd
Robert Shih <robertshih@google.com>
2019-11-25 22:25:42 -0800

    msm8996-common: Enable v1.3 clearkey drm plugin
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: Ie34e59947577988f75fae52d22e3f573f4b92420

--- msm8996.mk
+++ msm8996.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # For config.fs
 PRODUCT_PACKAGES += \
