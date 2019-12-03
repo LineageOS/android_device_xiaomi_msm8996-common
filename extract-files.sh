f0763235fefd3968eb2582959272d5dcaca15082
LuK1337 <priv.luk@gmail.com>
2019-12-03 19:57:22 +0100

    msm8996-common: "Fix" imsrcsd symbol dependencies
    Change-Id: I19308d9bb687731b77fd57e38da6c71d7104a4bd

--- extract-files.sh
+++ extract-files.sh
@@ -33,8 +33,20 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
+    vendor/bin/imsrcsd)
+        patchelf --replace-needed "libbase.so" "libbase-v28.so" "${2}"
+        patchelf --replace-needed "libhidlbase.so" "libhidlbase-v28.so" "${2}"
+        patchelf --replace-needed "libhidltransport.so" "libhidltransport-v28.so" "${2}"
+        patchelf --replace-needed "libhwbinder.so" "libhwbinder-v28.so" "${2}"
+        patchelf --add-needed "libbase.so" "${2}"
+        patchelf --add-needed "libhidlbase.so" "${2}"
+        ;;
     vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so)
         patchelf --replace-needed "libbase.so" "libbase-v28.so" "${2}"
+        ;;
+    vendor/lib64/lib-uceservice.so)
+        patchelf --replace-needed "libbase.so" "libbase-v28.so" "${2}"
+        patchelf --add-needed "libbase.so" "${2}"
         ;;
     esac
 }
