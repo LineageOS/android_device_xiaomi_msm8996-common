2f08827026f9936466be4b7c7f0e5b874799ffad
Vandana Jain <vandja@codeaurora.org>
2020-04-22 11:41:31 +0530

    msm8996-common: QCamera2: Remove libhidltransport empty library
    Remove unnecessary library dependencies in camera.
    
    External Impact: No.
    
    Change-Id: I428e005e98e7b037276399a392ba19bb5fd822ca

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -110,7 +110,7 @@ LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 ifeq ($(USE_DISPLAY_SERVICE),true)
-LOCAL_SHARED_LIBRARIES += android.frameworks.displayservice@1.0 libhidlbase libhidltransport
+LOCAL_SHARED_LIBRARIES += android.frameworks.displayservice@1.0 libhidlbase
 else
 LOCAL_SHARED_LIBRARIES += libgui
 endif
