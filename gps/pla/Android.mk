25e1aa93382bf86f2b85029dbbe787212197b5a7
Bruno Martins <bgcngm@gmail.com>
2020-04-26 21:04:56 +0100

    msm8996-common: Update GNSS HAL to Q
    QC package: LA.UM.8.6.r1-02600-89xx.0
    
    Change-Id: Ic86a40a44fbf9626cd05f10f1617d65c10831027

--- gps/pla/Android.mk
+++ gps/pla/Android.mk
@@ -1,7 +1,6 @@
 GNSS_CFLAGS := \
     -Werror \
     -Wno-error=unused-parameter \
-    -Wno-error=format \
     -Wno-error=macro-redefined \
     -Wno-error=reorder \
     -Wno-error=missing-braces \
@@ -16,9 +15,15 @@ GNSS_CFLAGS := \
     -Wno-error=switch \
     -Wno-error=date-time
 
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifneq ($(BUILD_TINY_ANDROID),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_pla_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/android
 include $(BUILD_HEADER_LIBRARY)
+
+endif # not BUILD_TINY_ANDROID
+endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
