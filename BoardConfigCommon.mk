64e384f5a614a242bdaa3e887156e3f540d26fa8
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    msm8996-common: Allow ELF prebuilts in PRODUCT_COPY_FILES.
     * ELF prebuilts were banned in PRODUCT_COPY_FILES starting
       May 11th. See b/156534160 for long-term fix which should
       override this change.
    
    Change-Id: Icd25b40ca4e4285319775046a1b70de30572be07
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,8 @@ BOARD_VENDOR := xiaomi
 
 VENDOR_PATH := device/xiaomi/msm8996-common
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
