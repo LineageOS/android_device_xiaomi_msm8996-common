55356702aa4366827198b1af0f9b9e1d4db0700e
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-10 12:10:21 -0400

    msm8996-common: bluetooth: Disable clean turn on
    * This isn't needed anymore.
    
    Change-Id: Ib77e5212099d82bbc07aea6044eb35109faff5cb

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -52,7 +52,6 @@ static inline const char* BtmGetDefaultName()
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Prefer non-WBS SCO */
 #define BTIF_HF_WBS_PREFERRED FALSE
