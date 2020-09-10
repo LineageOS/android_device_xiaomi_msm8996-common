df7c7d67b63e8d3f5369aa3af1df0a269f3a2b58
Davide Garberi <dade.garberi@gmail.com>
2020-09-10 03:57:15 +0200

    msm8996-common: Build libhidltransport and libhwbinder
    Change-Id: I1b4b255ce40c94e3bf2e6234b7280fc094a9270b
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -226,7 +226,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # HW crypto
 PRODUCT_PACKAGES += \
