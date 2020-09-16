d0771ccf5b10579074403bc3fb8798ec71651791
Lucchetto <lucchetto.tie@live.com>
2020-09-16 18:48:36 +0200

    msm8996-common: adapt libinit to Android 11
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>
    Change-Id: Ia6b6f753891b639dad4e380bdba6b594faf2a550

--- init/init_msm8996.cpp
+++ init/init_msm8996.cpp
@@ -36,8 +36,13 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 using android::base::GetProperty;
-using android::init::property_set;
+int property_set(const char *key, const char *value) {
+    return __system_property_set(key, value);
+}
 
 char const *heapminfree;
 char const *heapmaxfree;
