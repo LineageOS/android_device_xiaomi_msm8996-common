7577959a36696e8d16770d1151f7c6c89326f978
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    msm8996-common: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -188,7 +188,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2018-10-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
