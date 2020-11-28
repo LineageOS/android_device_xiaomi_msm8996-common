e89187c2a5412bf9f091087208ebf836b7196d15
Bruno Martins <bgcngm@gmail.com>
2020-11-28 01:17:08 +0000

    msm8996-common: Update ims.apk from daisy Q image
    Change-Id: I107aa37abdcbbe0581d0d9047d0e27a2eabafd9e

--- msm8996.mk
+++ msm8996.mk
@@ -320,8 +320,9 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/permissions/product-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/permissions/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
 
 # RCS
 PRODUCT_PACKAGES += \
