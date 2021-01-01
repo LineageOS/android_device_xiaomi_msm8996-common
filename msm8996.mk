0942f82e6b2c2e4c73fea4393c3f7add5b09917d
Michael Bestas <mkbestas@lineageos.org>
2021-01-01 04:57:09 +0200

    msm8996-common: Remove android.hardware.graphics.composer@2.1-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- msm8996.mk
+++ msm8996.mk
@@ -162,7 +162,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
