5f1ca515cd9f4d5650a04de1031fc533c5b57398
Bruno Martins <bgcngm@gmail.com>
2021-01-03 14:35:35 +0000

    msm8996-common: Inherit from AOSP product makefiles in the common tree
    This is common across all Mi8996 targets, so do it here instead.
    
    Change-Id: I47af66ca79b110f343dddf0b08e0b64af5516ee1

--- msm8996.mk
+++ msm8996.mk
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Get non-open-source specific aspects
