11eb4c7121aa53f39ef000c130125793aeda0dce
Bruno Martins <bgcngm@gmail.com>
2021-01-06 12:41:00 +0000

    msm8996-common: Introduce key swapper touch HAL
    Change-Id: Icec2b86bf58ff2f49c4d3ea3d3dfe0a7d4f43679

--- configpanel/src/org/lineageos/settings/device/Constants.java
+++ configpanel/src/org/lineageos/settings/device/Constants.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017-2018 The LineageOS Project
+ *           (C) 2017-2018,2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@
 
 package org.lineageos.settings.device;
 
-import org.lineageos.internal.util.FileUtils;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,16 +26,13 @@ public class Constants {
     public static final String CATEGORY_FP = "fp_key";
 
     // Preference keys
-    public static final String BUTTON_SWAP_KEY = "button_swap";
     public static final String FP_HOME_KEY = "fp_home";
     public static final String FP_POCKETMODE_KEY = "fp_pocketmode";
     public static final String FP_WAKEUP_KEY = "fp_wakeup";
 
     // Nodes
-    public static final String CYTTSP_BUTTON_SWAP_NODE = "/proc/buttons/reversed_keys_enable";
     public static final String FP_HOME_KEY_NODE = "/sys/devices/soc/soc:fpc_fpc1020/enable_key_events";
     public static final String FP_WAKEUP_NODE = "/sys/devices/soc/soc:fpc_fpc1020/enable_wakeup";
-    public static final String TOUCHPANEL_BUTTON_SWAP_NODE = "/proc/touchpanel/reversed_keys_enable";
     public static final String VIRTUAL_KEYS_NODE = "/proc/touchpanel/capacitive_keys_enable";
 
     // Intents
@@ -58,21 +53,14 @@ public class Constants {
     public static final Map<String, String[]> sNodeDependencyMap = new HashMap<>();
 
     public static final String[] sButtonPrefKeys = {
-        BUTTON_SWAP_KEY,
         FP_HOME_KEY,
         FP_WAKEUP_KEY
     };
 
     static {
-        if (FileUtils.fileExists(Constants.CYTTSP_BUTTON_SWAP_NODE)) {
-            sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, CYTTSP_BUTTON_SWAP_NODE);
-        } else if (FileUtils.fileExists(Constants.TOUCHPANEL_BUTTON_SWAP_NODE)) {
-            sBooleanNodePreferenceMap.put(BUTTON_SWAP_KEY, TOUCHPANEL_BUTTON_SWAP_NODE);
-        }
         sBooleanNodePreferenceMap.put(FP_HOME_KEY, FP_HOME_KEY_NODE);
         sBooleanNodePreferenceMap.put(FP_WAKEUP_KEY, FP_WAKEUP_NODE);
 
-        sNodeDefaultMap.put(BUTTON_SWAP_KEY, false);
         sNodeDefaultMap.put(FP_HOME_KEY, false);
         sNodeDefaultMap.put(FP_WAKEUP_KEY, true);
 
