4be6b9f0bfd3b1175ddc2ac77569b8a140616212
Bruno Martins <bgcngm@gmail.com>
2021-01-06 12:41:00 +0000

    msm8996-common: Introduce key swapper touch HAL interface
    As a consequence of adding this new interface, VINTF fragment
    needs to be dropped because not all devices may support it.
    
    Change-Id: Icec2b86bf58ff2f49c4d3ea3d3dfe0a7d4f43679

--- configpanel/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ configpanel/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *           (C) 2017-2019 The LineageOS Project
+ *           (C) 2017-2019,2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,10 +68,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     }
 
     static boolean hasButtonProcs() {
-        return (FileUtils.fileExists(Constants.CYTTSP_BUTTON_SWAP_NODE) ||
-                FileUtils.fileExists(Constants.FP_HOME_KEY_NODE) ||
-                FileUtils.fileExists(Constants.FP_WAKEUP_NODE) ||
-                FileUtils.fileExists(Constants.TOUCHPANEL_BUTTON_SWAP_NODE));
+        return (FileUtils.fileExists(Constants.FP_HOME_KEY_NODE) ||
+                FileUtils.fileExists(Constants.FP_WAKEUP_NODE));
     }
 
     private void disableComponent(Context context, String component) {
