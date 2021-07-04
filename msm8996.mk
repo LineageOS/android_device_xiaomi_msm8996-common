7338fcaadfee099a6184b69943f9c461796024fd
Marc Bourgoin <themard85@gmail.com>
2021-07-04 15:02:02 -0600

    msm8996-common: Drop QCOM WFD
    Change-Id: I3dd591444dc54ebe3a32e0385cbfc3db6bfc5e1f

--- msm8996.mk
+++ msm8996.mk
@@ -427,10 +427,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
