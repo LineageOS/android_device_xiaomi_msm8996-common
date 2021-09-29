807ef2241a58544e80994b4f2c624c95800d4d5f
sudharani <sshakunt@codeaurora.org>
2021-09-29 16:48:59 +0530

    msm8996-common: Set manufacturer and soc properties
    Set soc model and manufacturer properties.
    
    Change-Id: Ia75eb8862c7d879c40e64527d32d082bfa9b8e42

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -167,6 +167,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.df.iwlan_mux=9 \
     persist.data.df.dev_name=rmnet_usb0
 
+# SoC
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=MSM8996
+
 # TimeService
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
