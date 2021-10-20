a55ea3fdab122bbe45edcac5751de4ce8693ff42
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    msm8996-common: ims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- extract-files.sh
+++ extract-files.sh
@@ -39,6 +39,12 @@ function blob_fixup() {
     system_ext/etc/permissions/qti_libpermissions.xml)
         sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
         ;;
+    system_ext/lib64/lib-imscamera.so)
+        "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+        ;;
+    system_ext/lib64/lib-imsvt.so)
+        "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+        ;;
     system_ext/lib64/libdpmframework.so)
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
