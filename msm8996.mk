7148388e0dfabd6f42564a75e24ca212e8221e91
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    msm8996-common: ims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- msm8996.mk
+++ msm8996.mk
@@ -383,7 +383,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    telephony-ext \
+    libgui_shim
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
