1642dedcddec7f63cc62dc9a05a9c709093b208d
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2021-10-24 10:31:54 +0300

    msm8996-common: camera: Workaround compiling warnings
    : '(' and '{' tokens introducing statement expression appear in different macro
    expansion contexts [-Wcompound-token-split-by-macro]
                ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters, CAM_INTF_META_STREAM_INFO,
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    device/xiaomi/msm8996-common/camera/QCamera2/stack/common/cam_intf.h:685:6: note:
     expanded from macro 'ADD_SET_PARAM_ENTRY_TO_BATCH'
        ((LOGE("Unable to set metadata TABLE_PTR:%p META_ID:%d", \
         ^
    device/xiaomi/msm8996-common/camera/QCamera2/HAL3/QCamera3HWI.cpp:525:13: note: '
    {' token is here
                ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters, CAM_INTF_META_STREAM_INFO,
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    device/xiaomi/msm8996-common/camera/QCamera2/stack/common/cam_intf.h:685:7: note:
     expanded from macro 'ADD_SET_PARAM_ENTRY_TO_BATCH'
        ((LOGE("Unable to set metadata TABLE_PTR:%p META_ID:%d", \
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I597c6c9c5382910ffa1ea8ebe804cae02bb72543

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS := -Werror \
+LOCAL_CFLAGS := -Werror -Wno-compound-token-split-by-macro \
                    -g -O0
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../qexif
