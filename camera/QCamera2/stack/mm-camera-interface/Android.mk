64a013d8e8c01447a17a71dd964bda56af2c8b43
CrisBal <74150843+CBG-leeco@users.noreply.github.com>
2021-10-24 10:31:54 +0300

    msm8996-common: Camera: Fix compiling warnings
    : '(' and '{' tokens introducing statement expression appear in different macro
    expansion contexts [-Wcompound-token-split-by-macro]
                ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters, CAM_INTF_META_STREAM_INFO,
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    device/xiaomi/msm8996-common/camera/QCamera2/stack/common/cam_intf.h:685:6: note:
     expanded from macro 'ADD_SET_PARAM_ENTRY_TO_BATCH'
        ((LOGE("Unable to set metadata TABLE_PTR:%p META_ID:%d", \
         ^
    device/xiaomi/msm8996-common/camera/QCamera2/HAL3/QCamera3HWI.cpp:525:13: note: '
    {' token is here
                ADD_SET_PARAM_ENTRY_TO_BATCH(mParameters, CAM_INTF_META_STREAM_INFO,
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    device/xiaomi/msm8996-common/camera/QCamera2/stack/common/cam_intf.h:685:7: note:
     expanded from macro 'ADD_SET_PARAM_ENTRY_TO_BATCH'
        ((LOGE("Unable to set metadata TABLE_PTR:%p META_ID:%d", \
    
    [ 19% 3331/17092] target  C++: librecovery_updater_xiaomi <= device/xiaomi/msm8996
    device/xiaomi/msm8996-common/recovery/recovery_updater.cpp:176:10: warning: unuse
    d variable 'updater' [-Wunused-variable]
        auto updater = state->updater;
             ^
    device/xiaomi/msm8996-common/recovery/recovery_updater.cpp:212:40: warning: unuse
    d parameter 'name' [-Wunused-parameter]
    Value * GetDeviceVariantFn(const char *name, State *state, const std::vector<std
    ::unique_ptr<Expr>>& argv) {
                                           ^
    device/xiaomi/msm8996-common/recovery/recovery_updater.cpp:212:53: warning: unuse
    d parameter 'state' [-Wunused-parameter]
    Value * GetDeviceVariantFn(const char *name, State *state, const std::vector<std
    ::unique_ptr<Expr>>& argv) {
                                                        ^
    device/xiaomi/msm8996-common/recovery/recovery_updater.cpp:212:102: warning: unus
    ed parameter 'argv' [-Wunused-parameter]
    Value * GetDeviceVariantFn(const char *name, State *state, const std::vector<std
    ::unique_ptr<Expr>>& argv) {
    
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I597c6c9c5382910ffa1ea8ebe804cae02bb72543

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -41,7 +41,7 @@ ifneq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 17 ))" )))
   LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/un.h
 endif
 
-LOCAL_CFLAGS += -Wall -Wextra -Werror
+LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-compound-token-split-by-macro
 
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
