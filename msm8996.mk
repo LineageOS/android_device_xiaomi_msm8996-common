016c4acfabe4a2e2f25e72617e4312de0ec35487
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    msm8996-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- msm8996.mk
+++ msm8996.mk
@@ -191,8 +191,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.4-service.clearkey
 
