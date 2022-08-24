04ef0073c9a6a37194b12725360778d6b4ce6526
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    msm8996-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- msm8996.mk
+++ msm8996.mk
@@ -18,6 +18,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Enable updating of APEXes
