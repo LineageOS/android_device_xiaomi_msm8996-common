zuk-devs/android_device_zuk_msm8996-common

msm8996-common: Set manufacturer and soc properties
* Set soc model and manufacturer properties.

Change-Id: Ia75eb8862c7d879c40e64527d32d082bfa9b8e42

vendor_prop.mk
@@ -64,6 +64,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
 
+# Chipset
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=MSM8996
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1

LineageOS/android_device_xiaomi_msm8996-common

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

vendor_prop.mk
@@ -64,6 +64,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
 
+# Chipset
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.soc.manufacturer=Qualcomm \
+    ro.soc.model=MSM8998
+
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1