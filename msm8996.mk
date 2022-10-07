4b216e81b7d5bdec0dbed73ade5cce0bb8e0a2cb
Bruno Martins <bgcngm@gmail.com>
2022-10-08 00:53:19 +0100

    msm8996-common: Make camera stack treble compatible
    Change-Id: I21cc1c78b008ed096e62834afe730a5769b40ea9

--- msm8996.mk
+++ msm8996.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
+    libgui_vendor \
     libmm-qcamera \
     libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0
