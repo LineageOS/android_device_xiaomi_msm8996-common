a458dd35dcaddbe448f5c978dd82f6b107c7d4cd
Bruno Martins <bgcngm@gmail.com>
2022-10-08 16:29:00 +0100

    msm8996-common: Nuke init.panel.sh
    Change-Id: If2faf114270415303acdec72dcc20b7d013c93a4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,14 +3,6 @@ LOCAL_PATH:= $(call my-dir)
 # Common config scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.panel.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.panel.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
