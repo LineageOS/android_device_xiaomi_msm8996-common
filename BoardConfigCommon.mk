9f80c972b66e29303b355a603c7b733a1b90cb37
LuK1337 <priv.luk@gmail.com>
2022-10-08 02:31:02 +0200

    msm8996-common: Re-patch libmmcamera2_isp_modules for P's mutex restrictions
    Overriding SDK API level (see commit 1d0c99d) is no longer a thing
    since it wasn't ported over.
    
    Change-Id: I04763b77471ada315659f0452c70702daf604c5e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2021 The LineageOS Project
+# Copyright (C) 2017-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -95,8 +95,6 @@ QCOM_BT_USE_BTNV := true
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
-TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
-    /vendor/bin/mm-qcamera-daemon=26
 TARGET_SUPPORT_HAL1 := false
 TARGET_USES_MEDIA_EXTENSIONS := true
 
