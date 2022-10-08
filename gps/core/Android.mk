f3aa6c757f59261712282e02e7dbbd6e6a2a006e
Bruno Martins <bgcngm@gmail.com>
2022-10-08 23:42:55 +0100

    msm8996-common: Update GPS to LA.UM.8.6.r1-01900-89xx.0
    Change-Id: I50a85d9390b59bf59ff745d9a7e0f7a84b956d63

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -1,15 +1,13 @@
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+ifneq ($(BUILD_TINY_ANDROID),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_core
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
-
-ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
-LOCAL_CFLAGS += -DPDK_FEATURE_SET
-endif
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
@@ -17,19 +15,15 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libgps.utils \
     libdl \
-    liblog \
-    libloc_pla
+    liblog
 
 LOCAL_SRC_FILES += \
     LocApiBase.cpp \
     LocAdapterBase.cpp \
     ContextBase.cpp \
-    LocDualContext.cpp \
+    LocContext.cpp \
     loc_core_log.cpp \
     data-items/DataItemsFactoryProxy.cpp \
-    data-items/common/ClientIndex.cpp \
-    data-items/common/DataItemIndex.cpp \
-    data-items/common/IndexFactory.cpp \
     SystemStatusOsObserver.cpp \
     SystemStatus.cpp
 
@@ -43,6 +37,7 @@ LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/observer \
 
 LOCAL_HEADER_LIBRARIES := \
+    libutils_headers \
     libgps.utils_headers \
     libloc_pla_headers \
     liblocation_api_headers
@@ -59,3 +54,6 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
     $(LOCAL_PATH)/data-items/common \
     $(LOCAL_PATH)/observer
 include $(BUILD_HEADER_LIBRARY)
+
+endif # not BUILD_TINY_ANDROID
+endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
