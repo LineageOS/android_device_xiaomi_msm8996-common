e2db4cf7089e2265b391532af7936869b9fb3ec1
Bruno Martins <bgcngm@gmail.com>
2022-10-13 16:48:48 +0100

    msm8996-common: camera: Fix compilation errors
    device/xiaomi/msm8996-common/camera/QCamera2/HAL3/QCamera3Stream.cpp:58:9: error: suspicious concatenation of string literals in an array initialization; did you mean to separate the elements with a comm
    a? [-Werror,-Wstring-concatenation]
            "CAM_MAX" };
            ^
    device/xiaomi/msm8996-common/camera/QCamera2/HAL3/QCamera3Stream.cpp:57:9: note: place parentheses around the string literal to silence warning
            "CAM_ANALYSIS"
            ^
    
    Change-Id: I8834eb32d377db51a28c237e95f5f13db86e4998

--- camera/QCamera2/HAL3/QCamera3Stream.cpp
+++ camera/QCamera2/HAL3/QCamera3Stream.cpp
@@ -54,8 +54,7 @@ const char* QCamera3Stream::mStreamNames[] = {
         "CAM_RAW",
         "CAM_OFFLINE_PROC",
         "CAM_PARM",
-        "CAM_ANALYSIS"
-        "CAM_MAX" };
+        "CAM_ANALYSIS" };
 
 /*===========================================================================
  * FUNCTION   : get_bufs
