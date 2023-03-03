c52285d9d69e876aba32328bac7df13df1f5f80b
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    msm8996-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -254,6 +254,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
