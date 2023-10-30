bdf5119e83aa9d7badf0e272001e14b7be5954d6
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 17:38:30 +0200

    msm8996-common: Enforce vintf kernel requirements
    The build system enables this for shipping API level >=29
    but we also want it to pass the vintf runtime checks.
    
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca91c

--- msm8996.mk
+++ msm8996.mk
@@ -24,6 +24,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
 
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
