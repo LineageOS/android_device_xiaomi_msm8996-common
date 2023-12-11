5da0ad88ea9a468a9b5de7180695a4b08302c578
Bruno Martins <bgcngm@gmail.com>
2023-12-11 11:18:48 +0000

    msm8996-common: configpanel: Remove unnecessary AndroidX dep
    Spotted after A14 QPR1 merge as it breaks compilation.
    
    Change-Id: Ic5a10524471cfe2b37a0c367beb2454e7d3c6ad1

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -12,7 +12,6 @@ LOCAL_PRODUCT_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    androidx.core_core \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
