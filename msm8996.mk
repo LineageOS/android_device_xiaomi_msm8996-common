70b739bbe20d713dddb51e3154981377e23a0b40
Georg Veichtlbauer <georg@vware.at>
2024-06-25 23:02:34 +0200

    msm8996-common: Build vendor variant of libpower
    Change-Id: Id7fe8a2598ffb93be4635424c86100a88aea8780

--- msm8996.mk
+++ msm8996.mk
@@ -365,7 +365,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libpower.vendor
 
 # Shims
 PRODUCT_PACKAGES += \
