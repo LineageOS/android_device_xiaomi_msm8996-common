6992eb03ea0a8b89d1fcc0f258a3b651a30cae94
LuK1337 <priv.luk@gmail.com>
2024-11-01 13:58:10 +0100

    msm8996-common: Switch to libhidlbase_shim
    Change-Id: I1b31199ec389ea6ba0d8faea4c539839c1cb8170

--- extract-files.sh
+++ extract-files.sh
@@ -26,7 +26,7 @@ source "${HELPER}"
 function blob_fixup() {
     case "${1}" in
     system/lib64/com.qualcomm.qti.ant@1.0.so)
-        "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+        grep -q "libhidlbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
         ;;
     system_ext/etc/init/dpmd.rc)
         sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
@@ -53,7 +53,7 @@ function blob_fixup() {
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
     vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti|vendor/bin/hw/vendor.display.color@1.0-service)
-        "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+        grep -q "libhidlbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libhidlbase_shim.so" "${2}"
         ;;
     vendor/bin/pm-service)
         grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
