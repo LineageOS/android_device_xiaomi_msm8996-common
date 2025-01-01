ff1c22ac21489a30573b33acfb5ba28dfdcf32b0
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    msm8996-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- msm8996.mk
+++ msm8996.mk
@@ -177,7 +177,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     gralloc.msm8996 \
-    hwcomposer.msm8996 \
+    hwcomposer.qcom \
     libdisplayconfig \
     libqdMetaData.system \
     libtinyxml \
