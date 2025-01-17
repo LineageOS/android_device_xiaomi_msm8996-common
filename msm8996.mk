39608638d59feab4bb8e25af30c10192ad375024
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    msm8996-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- msm8996.mk
+++ msm8996.mk
@@ -265,7 +265,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # Lineage Health
 PRODUCT_PACKAGES += \
