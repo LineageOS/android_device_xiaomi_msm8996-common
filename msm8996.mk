42aa3a840340e534f536f69114acb52aadee58df
Michael Bestas <mkbestas@lineageos.org>
2025-01-27 12:44:55 +0330

    msm8996-common: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- msm8996.mk
+++ msm8996.mk
@@ -304,6 +304,12 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
