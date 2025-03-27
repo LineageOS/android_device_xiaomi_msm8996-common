67969af08fa491325a96ff7f2e092693a3abb663
LuK1337 <priv.luk@gmail.com>
2025-03-27 09:59:52 +0100

    msm8996-common: Use libcutils-v29 target
    Change-Id: Ie0b9f39412a3e3c711c28c5a78925720d85658c3

--- msm8996.mk
+++ msm8996.mk
@@ -399,10 +399,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
+    libcutils-v29 \
     libutils-v33
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so
 
 # WiFi
 PRODUCT_PACKAGES += \
