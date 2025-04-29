7633bc9899d7d6e66ecb2cdd842921110cb27d9a
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    msm8996-common: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- msm8996.mk
+++ msm8996.mk
@@ -399,6 +399,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
 
+# Vendor init
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit.xiaomi_8996)
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcutils-v29 \
