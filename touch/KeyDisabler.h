0e41cd6c35b4409fff2941fd837c24c5b989bf1d
Bruno Martins <bgcngm@gmail.com>
2025-06-07 09:25:14 +0100

    msm8996-common: touch: Migrate to AIDL
    Change-Id: I89fa58ae2eb5d7741bf5628d4f56ad0eb55e59ff

--- touch/KeyDisabler.h
+++ touch/KeyDisabler.h
@@ -1,45 +1,29 @@
 /*
- * Copyright (C) 2019,2021 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
-#include <vendor/lineage/touch/1.0/IKeyDisabler.h>
+#include <aidl/vendor/lineage/touch/BnKeyDisabler.h>
 
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
-namespace V1_0 {
-namespace implementation {
 
-using ::android::hardware::Return;
-
-class KeyDisabler : public IKeyDisabler {
+class KeyDisabler : public BnKeyDisabler {
   public:
     KeyDisabler();
 
-    // Methods from ::vendor::lineage::touch::V1_0::IKeyDisabler follow.
-    Return<bool> isEnabled() override;
-    Return<bool> setEnabled(bool enabled) override;
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
 
   private:
     bool has_key_disabler_;
 };
 
-}  // namespace implementation
-}  // namespace V1_0
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
+}  // namespace aidl
